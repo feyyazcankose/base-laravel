d6ecc903984f2838db2d67ec4135d46a1296a4e7
Feyyaz Can KÖSE <feyyazcan.kose@inity.com.tr>
2024-06-04 05:23:40 +0300

    D

--- app/Http/Controllers/Admin/User/UserController.php
+++ app/Http/Controllers/Admin/User/UserController.php
@@ -28,6 +28,41 @@ class UserController extends Controller
      *         response=200,
      *         description="Successful responses",
      *     )
+     * @OA\Parameter(
+     *     name="skip",
+     *     in="query",
+     *     required=false,
+     *     @OA\Schema(type="integer", example=1)
+     * )
+     * @OA\Parameter(
+     *     name="take",
+     *     in="query",
+     *     required=false,
+     *     @OA\Schema(type="integer", example=10)
+     * )
+     * @OA\Parameter(
+     *     name="sort",
+     *     in="query",
+     *     required=false,
+     *     @OA\Schema(type="string", example="yourSelector,asc")
+     * )
+     * @OA\Parameter(
+     *     name="filter",
+     *     in="query",
+     *     required=false,
+     *     description="Array string",
+     *     @OA\Schema(
+     *         type="string",
+     *     )
+     * )
+     * @OA\Parameter(
+     *     name="group",
+     *     in="query",
+     *     required=false,
+     *     description="Array object",
+     *     @OA\Schema(
+     *         type="string",
+     *     )
      * )
      */
     public function gets(Request $request)
