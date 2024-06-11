878ded36b2b8cd2717b42008a1dbec177fb07bc0
Feyyaz Can KÖSE <feyyazcan.kose@inity.com.tr>
2024-06-12 00:09:09 +0300

    Bug Fix

--- dashboard/src/base/components/common/dynamo-table/components/SortDropdown.tsx
+++ dashboard/src/base/components/common/dynamo-table/components/SortDropdown.tsx
@@ -16,82 +16,104 @@ type SortDropdownProps = {
     setSort: React.Dispatch<React.SetStateAction<string | undefined>>;
 };
 
-function SortDropdown({ column, filterChain, sort, setSort }: Readonly<SortDropdownProps>) {
-   
+function SortDropdown({
+    column,
+    filterChain,
+    sort,
+    setSort,
+}: Readonly<SortDropdownProps>) {
     const sortAscending = () => {
-        // Logic to sort ascending
+        // Logic to Artan Sıralama
         setSort(`${column.key},asc`);
     };
 
     const sortDescending = () => {
-        // Logic to sort descending
+        // Logic to Azalan Sıralama
         setSort(`${column.key},desc`);
     };
 
     const sortDefault = () => {
-        // Logic to sort default
+        // Logic to Varsayılan Sırala
         setSort(undefined);
     };
 
-
-    return (
-        column.key === undefined ? (
-            <div className="flex justify-center items-center gap-1 cursor-default">
-                <span>{column.label}</span>
-            </div>
-        ) : (
+    return column.key === undefined ? (
+        <div className="flex justify-center items-center gap-1 cursor-default">
+            <span>{column.label}</span>
+        </div>
+    ) : (
         <Dropdown>
             <DropdownTrigger>
-                <div className= {clsx("flex justify-center items-center gap-1", {
-                            "text-primary-500": filterChain.some(
-                                (filter) => filter.id === column.key
-                            ),
-                        })}>
-                    <span
-                        className="cursor-pointer"
-                    >
-                        {column.label}
-                    </span>
+                <div
+                    className={clsx("flex justify-center items-center gap-1", {
+                        "text-primary-500": filterChain.some(
+                            (filter) => filter.id === column.key
+                        ),
+                    })}
+                >
+                    <span className="cursor-pointer">{column.label}</span>
                     {sort?.includes(column?.key) ? (
                         sort?.includes(",asc") ? (
-                            <Icon icon="bx:sort-a-z" width="1.2rem" height="1.2rem" />
+                            <Icon
+                                icon="bx:sort-a-z"
+                                width="1.2rem"
+                                height="1.2rem"
+                            />
                         ) : (
-                            <Icon icon="bx:sort-z-a" width="1.2rem" height="1.2rem" />
+                            <Icon
+                                icon="bx:sort-z-a"
+                                width="1.2rem"
+                                height="1.2rem"
+                            />
                         )
                     ) : null}
                 </div>
             </DropdownTrigger>
-            <DropdownMenu variant="faded" aria-label="Dropdown menu with description">
+            <DropdownMenu
+                variant="faded"
+                aria-label="Dropdown menu with description"
+            >
                 <DropdownItem
                     key="new"
                     startContent={
-                        <Icon icon="bx:sort-a-z" width="1.2rem" height="1.2rem" />
+                        <Icon
+                            icon="bx:sort-a-z"
+                            width="1.2rem"
+                            height="1.2rem"
+                        />
                     }
                     onClick={sortAscending}
                 >
-                    Sort Ascending
+                    Artan Sıralama
                 </DropdownItem>
                 <DropdownItem
                     key="copy"
                     startContent={
-                        <Icon icon="bx:sort-z-a" width="1.2rem" height="1.2rem" />
+                        <Icon
+                            icon="bx:sort-z-a"
+                            width="1.2rem"
+                            height="1.2rem"
+                        />
                     }
                     onClick={sortDescending}
                 >
-                    Sort Descending
+                    Azalan Sıralama
                 </DropdownItem>
                 <DropdownItem
                     key="edit"
                     startContent={
-                        <Icon icon="bxs:sort-alt" width="1.2rem" height="1.2rem" />
+                        <Icon
+                            icon="bxs:sort-alt"
+                            width="1.2rem"
+                            height="1.2rem"
+                        />
                     }
                     onClick={sortDefault}
                 >
-                    Sort Default
+                    Varsayılan Sırala
                 </DropdownItem>
             </DropdownMenu>
         </Dropdown>
-        )
     );
 }
 
